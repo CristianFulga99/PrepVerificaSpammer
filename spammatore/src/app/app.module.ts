804207b9d9bb6544c4f27dd77ee7433b4204be47
Cristian Fulga <cristian.fulga99@gmail.com>
2020-01-13 12:17:45 +0000

    Progetto completo con Bootstrap

--- spammatore/src/app/app.module.ts
+++ spammatore/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { FraseComponent } from './frase/frase.component';
 
@@ -10,7 +10,7 @@ import { FraseComponent } from './frase/frase.component';
     FraseComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
